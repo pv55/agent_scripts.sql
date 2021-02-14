d89a80f78ae92a166022b3f3c9c80ae759037943
pv55 <78259815+pv55@users.noreply.github.com>
2021-02-14 17:22:55 +0300

    Update Scouts_agregation_cumsum.sql
    Upd agent_leads

--- Scouts_agregation_cumsum.sql
+++ Scouts_agregation_cumsum.sql
@@ -72,6 +72,8 @@ and a.FTR_plus_30days >= date '2021-02-01'
 GROUP by a.city,a.driver_gk,a.cost_total,a.courier_type,a.phone,a.driver_name,a.driver_computed_rating,a.fleet_gk,a.driver_status,a.status,a.registration_date_key,a.ftp_date_key_all,a.ftp_date_key_park,a.FTR_plus_30days,a.ltp_date_key))
 
 (SELECT
+(case when s.city = 'МСК-Шараев' then 'Сергей Шараев' else f4.name_agent end) name_agent,
+(case when s.city = 'МСК-Шараев' then '900' else f4.utm_content end) utm_content,
 s.*,
 (case when s.ltp_date_different_park >= date '1900-01-01' then (case when date_diff('day', s.ltp_date_different_park,s.ftp_date_key_park) <= 59 then 'NoReFTR' else 'ReFTR' end ) else 'FTR' end) as type_couriers,
 (case when f3.date_pay <> '' then date(f3.date_pay) end) as last_pay,
@@ -80,24 +82,24 @@ s.*,
 
     else (case
 
-    when cast (f3.cumsum as integer) = 1800 then (case when s.All_rides_30_days >= 30 then cast (f3.cumsum as integer) + 1500 else 0 end)
+    when cast (f3.cumsum as integer) = 1800 then (case when s.All_rides_total >= 30 then cast (f3.cumsum as integer) + 1500 else 0 end)
     when cast (f3.cumsum as integer) = 3300 then 0
     when cast (f3.cumsum as integer) = 4000 then 0
-    when ftp_date_key_park <= date '2020-12-20' then (case when s.All_rides_30_days between 5 and 14 then (case when cast (f3.cumsum as integer) >= 0 then 500 - cast (f3.cumsum as integer) else 500 end) else 0 end )
-    when ftp_date_key_park <= date '2020-12-20' then (case when s.All_rides_30_days between 15 and 29 then 1800 else 0 end)
-    when ftp_date_key_park <= date '2020-12-20' then (case when s.All_rides_30_days >= 30 then 3300 else 0 end)
-    when s.All_rides_30_days <= 4 then 0
+    when ftp_date_key_park <= date '2020-12-20' then (case when s.All_rides_total between 5 and 14 then (case when cast (f3.cumsum as integer) >= 0 then 500 - cast (f3.cumsum as integer) else 500 end) else 0 end )
+    when ftp_date_key_park <= date '2020-12-20' then (case when s.All_rides_total between 15 and 29 then 1800 else 0 end)
+    when ftp_date_key_park <= date '2020-12-20' then (case when s.All_rides_total >= 30 then 3300 else 0 end)
+    when s.All_rides_total <= 4 then 0
 
-    when s.All_rides_30_days between 5 and 14 then (case when cast (f3.cumsum as integer) >= 0 then 500 - cast (f3.cumsum as integer) else 500 end)
-    when s.All_rides_30_days between 15 and 29 then (case when cast (f3.cumsum as integer) >= 0 then 2100 - cast (f3.cumsum as integer) else 2100 end)
-    when s.All_rides_30_days between 15 and 29 then (case when cast (f3.cumsum as integer) >= 0 then 2100 - cast (f3.cumsum as integer) else 2100 end)
-    when s.All_rides_30_days >= 30 then (case when cast (f3.cumsum as integer) >= 0 then 4000 - cast (f3.cumsum as integer) else 4000 end)
+    when s.All_rides_total between 5 and 14 then (case when cast (f3.cumsum as integer) >= 0 then 500 - cast (f3.cumsum as integer) else 500 end)
+    when s.All_rides_total between 15 and 29 then (case when cast (f3.cumsum as integer) >= 0 then 2100 - cast (f3.cumsum as integer) else 2100 end)
+    when s.All_rides_total between 15 and 29 then (case when cast (f3.cumsum as integer) >= 0 then 2100 - cast (f3.cumsum as integer) else 2100 end)
+    when s.All_rides_total >= 30 then (case when cast (f3.cumsum as integer) >= 0 then 4000 - cast (f3.cumsum as integer) else 4000 end)
 
     else 0 end ) end) Cumsum
 
 from Payment s
 left join sheets.default.Payments_Scouts f3  on cast(f3.id as integer) = cast(substring(cast(s.driver_gk as varchar), 4) as integer)
-
+left join sheets.default.leads_Scouts f4  on f4.phone = s.phone
 where 1=1
 
 )
